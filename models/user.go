86884da147a1138fb776a5a9ac3d75762436c372
rezvanmj <rezvan.mjj@gmail.com>
2025-11-16 17:14:11 +0330

    budget and expense model added

--- models/user.go
+++ models/user.go
@@ -4,6 +4,10 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Email    string `json:"email" gorm:"uniqueIndex"` //unique email
-	Password string `json:"password"`
+	Email        string `json:"email" gorm:"uniqueIndex"` //unique email
+	Password     string `json:"password"`
+	Income       int    `json:"income"`
+	SavingGoal   int    `json:"savingGoal"`
+	SavedMoney   int    `json:"savedMoney"`
+	CurrentMoney int    `json:"currentMoney"`
 }
